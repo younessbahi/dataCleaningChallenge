49d109241a0298ae4af95c4bac1ec65d76221af6
younessbahi <77124184+younessbahi@users.noreply.github.com>
2021-09-28 14:18:37 +0100

    minor change

--- dataCleaning.R
+++ dataCleaning.R
@@ -11,6 +11,7 @@ indexHeaders <- which(data$Row.Type == 'Row Type')
 rmHeaders    <- data[- indexHeaders, - c(10, 11)] %>% tibble()
 rmEmptyRows  <- rmHeaders[!apply(rmHeaders == "", 1, all),]
 
+# Return index location of the unwanted subHeaders [first name, last name, date]
 indexPerson <- which(
 		str_detect(
 			rmEmptyRows$Row.Type,
@@ -22,12 +23,12 @@ data <- rmEmptyRows %>%
 		mutate(
 			FirstName = NA,
 			LastName  = NA,
-			date      = NA
+			Date      = NA
 		)
 
 data[indexPerson, "FirstName"] <- data[indexPerson, 1]
 data[indexPerson, "LastName"]  <- data[indexPerson, 2]
-data[indexPerson, "date"]      <- data[indexPerson, 3]
+data[indexPerson, "Date"]      <- data[indexPerson, 3]
 
 data <- data %>%
 	tidyr::fill(
@@ -37,11 +38,11 @@ data <- data %>%
 
 data$FirstName <- stringr::str_remove_all(data$FirstName, pattern = 'first name: ')
 data$LastName  <- stringr::str_remove_all(data$LastName, pattern = 'last name: ')
-data$date      <- stringr::str_remove_all(data$date, pattern = 'date: ')
+data$date      <- stringr::str_remove_all(data$date, pattern = 'Date: ')
 
 cleaned <- data[- indexPerson,] %>%
 		dplyr::relocate(
 			FirstName,
 			LastName,
-			date
+			Date
 		)
