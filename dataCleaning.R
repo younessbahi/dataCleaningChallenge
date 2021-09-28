e108e28363e1fc313d5bc889054953c04a07378b
younessbahi <77124184+younessbahi@users.noreply.github.com>
2021-09-28 14:02:38 +0100

    First commit

---
+++ dataCleaning.R
@@ -0,0 +1,46 @@
+pacman::p_load(dplyr, data.table, stringr, tidyr)
+
+data             <- read.csv(
+		'../data_cleaning_challenge.csv',
+		stringsAsFactors = FALSE,
+		header           = TRUE
+)
+
+indexHeaders <- which(data$Row.Type == 'Row Type')
+rmHeaders    <- data[- indexHeaders, - c(10, 11)] %>% tibble()
+rmEmptyRows  <- rmHeaders[!apply(rmHeaders == "", 1, all),]
+
+indexPerson <- which(
+		str_detect(
+				rmEmptyRows$Row.Type,
+				'first name'
+		)
+)
+
+data <- rmEmptyRows %>%
+		mutate(
+				FirstName = NA,
+				LastName  = NA,
+				date      = NA
+		)
+
+data[indexPerson, "FirstName"] <- data[indexPerson, 1]
+data[indexPerson, "LastName"]  <- data[indexPerson, 2]
+data[indexPerson, "date"]      <- data[indexPerson, 3]
+
+data <- data %>%
+		tidyr::fill(
+				FirstName:date,
+				.direction = 'down'
+		)
+
+data$FirstName <- stringr::str_remove_all(data$FirstName, pattern = 'first name: ')
+data$LastName  <- stringr::str_remove_all(data$LastName, pattern = 'last name: ')
+data$date      <- stringr::str_remove_all(data$date, pattern = 'date: ')
+
+cleaned <- data[- indexPerson,] %>%
+		dplyr::relocate(
+				FirstName,
+				LastName,
+				date
+		)
