c340edb6db29bde699584183b595bb373a25071e
younessbahi <77124184+younessbahi@users.noreply.github.com>
2021-09-28 14:30:05 +0100

    Adding comments

--- dataCleaning.R
+++ dataCleaning.R
@@ -6,26 +6,28 @@ data <- read.csv(
 		stringsAsFactors = FALSE,
 		header           = TRUE
 )
-
+# Return row index location of the unwanted subHeaders ----
 indexHeaders <- which(data$Row.Type == 'Row Type')
+#Remove empty cols ----
 rmHeaders    <- data[- indexHeaders, - c(10, 11)] %>% tibble()
+# Remove empty rows ----
 rmEmptyRows  <- rmHeaders[!apply(rmHeaders == "", 1, all),]
 
-# Return index location of the unwanted subHeaders [first name, last name, date]
+# Return row index location persona info [first name, last name, date],  ----
 indexPerson <- which(
 		str_detect(
 			rmEmptyRows$Row.Type,
 			'first name'
 		)
 )
-
+# Create new empty cols for persona info ----
 data <- rmEmptyRows %>%
 		mutate(
 			FirstName = NA,
 			LastName  = NA,
 			Date      = NA
 		)
-
+# inject persona info to newly created cols ----
 data[indexPerson, "FirstName"] <- data[indexPerson, 1]
 data[indexPerson, "LastName"]  <- data[indexPerson, 2]
 data[indexPerson, "Date"]      <- data[indexPerson, 3]
@@ -35,7 +37,7 @@ data <- data %>%
 		FirstName:date,	
 		.direction = 'down'
 	)
-
+# Returns relevent persona info ----
 data$FirstName <- stringr::str_remove_all(data$FirstName, pattern = 'first name: ')
 data$LastName  <- stringr::str_remove_all(data$LastName, pattern = 'last name: ')
 data$date      <- stringr::str_remove_all(data$date, pattern = 'Date: ')
